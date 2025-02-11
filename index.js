3a4b9a1de796f8e1a5d4e9e4e567b1f4defc0ab3
xxsirr <oldmyr0@gmail.com>
2025-02-11 16:28:15 +0300

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["▶𝚈𝚘𝚞𝚝𝚞𝚋𝚎 Oldmyr", "𝙰𝚋𝚘𝚗𝚎 𝙾𝚕"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
